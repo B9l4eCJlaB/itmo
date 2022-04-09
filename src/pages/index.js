4c7bd2b66962ae9f987669218567222c9cec177c
julia953197 <julia953197@yandex.ru>
2022-04-09 13:07:21 +0300

    feat: простой header и навигация

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+const headerMenu = document.querySelector('.header__menu');
+const headerLogo = document.querySelector('.header__logo');
+const headerCloseButton = document.querySelector('.header__close-button');
+const navigationBar = document.querySelector('.navigation');
+const menuPath = "../images/burger-menu.svg";
+const logoPath = "../images/logo.svg";
+
+// Слушатель на открытие меню header
+headerMenu.addEventListener('click', function (){
+  // Проверка, что меню уже открыто
+  if(headerCloseButton.classList.contains('hidden-class')){
+    headerMenu.src = logoPath;
+    headerMenu.style.width = "71px";
+    headerLogo.classList.toggle('hidden-class');
+    headerCloseButton.classList.toggle('hidden-class');
+    navigationBar.style.display="block";
+  }
+});
+
+// Слушатель на закрытие меню header
+headerCloseButton.addEventListener("click", function(){
+  headerMenu.src = menuPath;
+  headerMenu.style.width = "24px";
+  headerLogo.classList.toggle('hidden-class');
+  headerCloseButton.classList.toggle('hidden-class');
+  navigationBar.style.display = "none";
+});
